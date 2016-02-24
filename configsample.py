11099b32570b54879a767c5f472b613bb5053f65
DanielLevi <dlevi18@gmail.com>
2016-02-23 22:28:17 -0500

    First Commit

---
+++ configsample.py
@@ -0,0 +1,6 @@
+## Config Settings
+
+myValue1 = "hello, world"
+myValue2 = "What's up?"
+myValue3 = "seems like it's working"
+myValue4 = 3829
---
+++ test.py
@@ -0,0 +1,7 @@
+from myconfig import *
+
+print myValue1
+print myValue2
+print myValue3
+print myValue4
+
